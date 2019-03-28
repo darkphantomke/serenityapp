15600970c0d4df87d69f94a4dff57e7238ad1f5a
darkphantomke@gmail.com <darkphantomke@gmail.com>
2019-03-28 19:13:01 +0300

    New commit

---
+++ public/main.js
@@ -0,0 +1,187 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"],{
+
+/***/ "./src/$$_lazy_route_resource lazy recursive":
+/*!**********************************************************!*\
+  !*** ./src/$$_lazy_route_resource lazy namespace object ***!
+  \**********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncaught exception popping up in devtools
+	return Promise.resolve().then(function() {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
+
+/***/ }),
+
+/***/ "./src/app/app.component.css":
+/*!***********************************!*\
+  !*** ./src/app/app.component.css ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2FwcC5jb21wb25lbnQuY3NzIn0= */"
+
+/***/ }),
+
+/***/ "./src/app/app.component.html":
+/*!************************************!*\
+  !*** ./src/app/app.component.html ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<!-- Start Navigation -->\n<nav class=\"navbar navbar-inverse navbar-fixed no-background nav-static bootsnav\" style=\"font-family: 'Quicksand', sans-serif\">\n  <!-- Start Top Search -->\n  <div class=\"top-search\">\n      <div class=\"container\">\n          <div class=\"input-group\">\n              <span class=\"input-group-addon\"><i class=\"fa fa-search\"></i></span>\n              <input type=\"text\" class=\"form-control\" placeholder=\"Type something ann hit enter\">\n              <span class=\"input-group-addon close-search\"><i class=\"fa fa-times\"></i></span>\n          </div>\n      </div>\n  </div>\n  <!-- End Top Search -->\n\n  <div class=\"container\">            \n      <!-- Start Atribute Navigation -->\n      <div class=\"attr-nav\">\n          <ul>\n              <li class=\"side-menu\"><a href=\"#\"><i class=\"fa fa-bars\"></i></a></li>\n          </ul>\n      </div>\n      <!-- End Atribute Navigation -->\n\n      <!-- Start Header Navigation -->\n      <div class=\"navbar-header\">\n          <a class=\"navbar-brand\" href=\"/\"><img src=\"../assets/img/brand/serenity_logo.svg\" class=\"logo\" alt=\"\"></a>\n      </div>\n      <!-- End Header Navigation -->\n  </div>   \n  \n  <!-- Start Side Menu -->\n  <div class=\"side\">\n      <a href=\"#\" class=\"close-side\"><i class=\"fa fa-times\"></i></a>\n      <div class=\"widget text-center\">\n          <img src=\"../assets/img/brand/logo2.0.png\" class=\"img-circle marginbot20\" alt=\"\" />\n          <h5 class=\"title\">About us</h5>\n          <p style=\"font-family: 'Work Sans', sans-serif; text-align:justify\">\n              Serenity Security Solutions is a security and fire systems specialist that has been protecting the nation’s homes and businesses since 2017. Since our inception, Serenity Security Solutions has been working diligently to provide solutions and services at the highest standards. \n              We provide service(s) within Nairobi.<br /><br />\n              Get in touch: 0722 123 456\n          </p>\n      </div>\n      <div class=\"widget text-center\">\n          <h5 class=\"title\">Join the community</h5>\n          <!-- Start Subscribe -->\n          <a rel=\"canonical\" href=\"https://twitter.com/Serenity_Kenya\" target=\"_blank\" class=\"btn btn-primary btn-sm btn-block btn-picton btn-icon\"><i class=\"fa fa-twitter\"></i> Twitter</a>\n          <a rel=\"canonical\" href=\"https://web.facebook.com/SerenitySolutionsOfficial/?view_public_for=1153103841481473\" target=\"_blank\" class=\"btn btn-sm btn-block btn-turquoise btn-icon\"><i class=\"fa fa-facebook\"></i> Facebook</a>\n          <!-- End Subscribe -->\n      </div>\n      <div class=\"col-md-12 col-sm-12\">\n          <h6 style=\"font-family: 'Josefin Slab', serif; color: #ffffff; text-align:center\">© 2017-2019 Serenity Security Solutions.&nbsp; All Rights Reserved.</h6>\n      </div>\n  </div>\n  <!-- End Side Menu -->\n</nav>\n<!-- End Navigation -->\n\n<!-- START REVOLUTION SLIDER 5.0 -->\n<div id=\"slider_container\" class=\"rev_slider_wrapper\">\n  <div id=\"rev-slider\" class=\"rev_slider\"  data-version=\"5.0\">\n      <ul> \n          <li data-transition=\"slidedown\">\n              <!-- MAIN IMAGE -->\n              <img src=\"../assets/img/slider/dialo_3.png\"  alt=\"\"  data-bgposition=\"center center\" data-bgfit=\"cover\" data-bgrepeat=\"no-repeat\" data-bgparallax=\"10\" class=\"rev-slidebg\" data-no-retina>\n              \n              <!-- LAYER NR. 1 -->\n              <div class=\"tp-caption cap-boxed single-boxedTitle capUppercase-title\" \n                  id=\"slide-1-layer-1\" \n                  data-x=\"left\" data-hoffset=\"['0','0','0','0']\" \n                  data-y=\"middle\" data-voffset=\"['0','0','0','0']\" \n                  data-width=\"['full','full','full','full']\"\n                  data-height=\"['full','full','full','full']\"\n                  data-transform_idle=\"o:1;\"\n                  data-transform_in=\"z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;s:1000;e:Power2.easeOut;\" \n                  data-transform_out=\"s:1000;e:Power3.easeInOut;s:1000;e:Power3.easeInOut;\" \n                  data-textAlign=\"['center','center','center','center']\"\n                  data-whitespace=\"nowrap\"\n                  data-basealign=\"slide\" \n                  data-type=\"shape\" \n                  data-fontsize=\"['76','63','48','29']\"\n                  data-lineheight=\"['76','62','48','29']\"\n                  data-paddingtop=\"[70,50,40,30]\"\n                  data-paddingright=\"[10,10,10,8]\"\n                  data-paddingbottom=\"[70,50,40,30]\"\n                  data-paddingleft=\"[10,10,10,8]\"\n                  data-start=\"500\" \n                  data-splitin=\"none\" \n                  data-splitout=\"none\" \n                  data-responsive_offset=\"on\" \n                  style=\"z-index: 4;font-weight:300; white-space: nowrap;\"></div>\t\n\n              <!-- LAYER NR. 2 -->\n              <div class=\"tp-caption white-text\" \n                  id=\"slide-1-layer-2\" \n                  data-x=\"left\" data-hoffset=\"['60','50','40','20']\" \n                  data-y=\"center\" data-voffset=\"['-40','-50','-70','-35']\" \n                  data-width=\"['620','512','390','400']\"\n                  data-height=\"['auto','auto','auto','auto']\"\n                  data-transform_idle=\"o:1;\"\n                  data-transform_in=\"y:-90;s:2000;e:Power4.easeInOut;\" \n                  data-transform_out=\"s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;\" \n                  data-textAlign=\"['left','left','left','left']\"\n                  data-whitespace=\"normal\"\n                  data-type=\"text\" \n                  data-fontsize=\"['64','48','36','24']\"\n                  data-lineheight=\"['82','64','48','24']\"\n                  data-start=\"1000\" \n                  data-splitin=\"none\" \n                  data-splitout=\"none\" \n                  data-responsive_offset=\"on\" \n                  style=\"z-index: 5; font-weight:700;white-space: normal; font-family: 'Quicksand', sans-serif\">Sorry... our site is under construction!\n              </div>\n              \n              <!-- Start Countdown -->\n              <div class=\"tp-caption countdown-wrapp\" \n                  id=\"slide-1-layer-3\" \n                  data-x=\"left\" data-hoffset=\"['60','50','40','20']\" \n                  data-y=\"center\" data-voffset=\"['140','110','70','50']\" \n                  data-width=\"['620','512','700','400']\"\n                  data-height=\"['auto','auto','auto','auto']\"\n                  data-transform_idle=\"o:1;\"\n                  data-transform_in=\"y:90;s:2000;e:Power4.easeInOut;\" \n                  data-transform_out=\"s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;\" \n                  data-textAlign=\"['left','left','left','left']\"\n                  data-whitespace=\"normal\"\n                  data-type=\"text\" \n                  data-fontsize=\"['64','48','36','18']\"\n                  data-lineheight=\"['82','64','48','24']\"\n                  data-start=\"2000\" \n                  data-splitin=\"none\" \n                  data-splitout=\"none\" \n                  data-responsive_offset=\"on\" \n                  style=\"z-index: 5; font-weight:700;white-space: normal; font-family: 'Quicksand', sans-serif\">\n                  <h5>We'll be up in:</h5>\n                  <div class=\"main-example\">\n                      <div class=\"countdown-container\" id=\"main-example\"></div>\n                  </div>\n              </div>\n              <!-- End Countdown -->\n          </li>\n      </ul>\n  </div>\n</div>\n<!-- END OF SLIDER WRAPPER -->\n\n\n"
+
+/***/ }),
+
+/***/ "./src/app/app.component.ts":
+/*!**********************************!*\
+  !*** ./src/app/app.component.ts ***!
+  \**********************************/
+/*! exports provided: AppComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppComponent", function() { return AppComponent; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.title = 'serenity-app';
+    }
+    AppComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-root',
+            template: __webpack_require__(/*! ./app.component.html */ "./src/app/app.component.html"),
+            styles: [__webpack_require__(/*! ./app.component.css */ "./src/app/app.component.css")]
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.module.ts":
+/*!*******************************!*\
+  !*** ./src/app/app.module.ts ***!
+  \*******************************/
+/*! exports provided: AppModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppModule", function() { return AppModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/platform-browser */ "./node_modules/@angular/platform-browser/fesm5/platform-browser.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
+
+
+
+
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_2__["NgModule"])({
+            declarations: [
+                _app_component__WEBPACK_IMPORTED_MODULE_3__["AppComponent"]
+            ],
+            imports: [
+                _angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__["BrowserModule"]
+            ],
+            providers: [],
+            bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_3__["AppComponent"]]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/environments/environment.ts":
+/*!*****************************************!*\
+  !*** ./src/environments/environment.ts ***!
+  \*****************************************/
+/*! exports provided: environment */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+var environment = {
+    production: false
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+/***/ }),
+
+/***/ "./src/main.ts":
+/*!*********************!*\
+  !*** ./src/main.ts ***!
+  \*********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/platform-browser-dynamic */ "./node_modules/@angular/platform-browser-dynamic/fesm5/platform-browser-dynamic.js");
+/* harmony import */ var _app_app_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/app.module */ "./src/app/app.module.ts");
+/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./environments/environment */ "./src/environments/environment.ts");
+
+
+
+
+if (_environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].production) {
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["enableProdMode"])();
+}
+Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformBrowserDynamic"])().bootstrapModule(_app_app_module__WEBPACK_IMPORTED_MODULE_2__["AppModule"])
+    .catch(function (err) { return console.error(err); });
+
+
+/***/ }),
+
+/***/ 0:
+/*!***************************!*\
+  !*** multi ./src/main.ts ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! E:\meanstack2.0\project2\serenity-app\serenity-app\src\main.ts */"./src/main.ts");
+
+
+/***/ })
+
+},[[0,"runtime","vendor"]]]);
+//# sourceMappingURL=main.js.map
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const https = require('https');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || 'localhost';
+const uri = 'mongodb://localhost:27017/twerk';
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(uri, { useNewUrlParser: true }, (err) => {
+    if (err) {
+        console.log(`Not connected to the database ${err}`);
+    } else {
+        console.log('SUCCESS: Connected to MongoDB.')
+    }
+});
+
+https.createServer((req, res) => {
+    res.writeHead(200, app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '/public/index.html'));
+    }) );
+    res.end();
+  }), app.listen(port, host, () => 
+  console.log(`App started on port https://${host}:${port}`)
+);
