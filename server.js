45ebb616ceb0afd7b6064a4f97f35340b2b57b03
darkphantomke@gmail.com <darkphantomke@gmail.com>
2019-03-29 15:17:34 +0300

    third

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const https = require('https');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 5000;
-//const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || 'localhost';
 const uri = 'mongodb://localhost:27017/twerk';
 const morgan = require('morgan');
 const mongoose = require('mongoose');
@@ -21,15 +21,11 @@ mongoose.connect(uri, { useNewUrlParser: true }, (err) => {
     }
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-}); 
-
-
-
-https.createServer(options, (req, res) => {
-    res.writeHead(200);
-    res.end('');
-  }, app).listen(port, () => 
-  console.log(`Server started on port ${port}`)
+https.createServer((req, res) => {
+    res.writeHead(200, app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '/public/index.html'));
+    }) );
+    res.end();
+  }), app.listen(port, host, () => 
+  console.log(`App started on port https://${host}:${port}`)
 );
