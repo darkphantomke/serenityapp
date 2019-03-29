b16b527c74ff9244dc6064674de0e1039e13a446
darkphantomke <46133578+darkphantomke@users.noreply.github.com>
2019-03-29 16:49:39 +0300

    correction

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 const host = process.env.HOST || 'localhost';
 const uri = 'mongodb://localhost:27017/twerk';
 const morgan = require('morgan');
@@ -17,14 +17,16 @@ mongoose.connect(uri, { useNewUrlParser: true }, (err) => {
     if (err) {
         console.log(`Not connected to the database ${err}`);
     } else {
-        console.log('SUCCESS: Connected to MongoDB.')
+        console.log('SUCCESS: Connected to MongoDB...')
     }
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
 http.createServer((req, res) => {
-    res.writeHead(200, app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '/public/index.html'));
-    }) );
+    res.writeHead(200);
     res.end();
   }), app.listen(port, host, () => 
   console.log(`App started on port https://${host}:${port}`)
