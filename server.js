5af6fbe54c0bee1cf74c1c5f5ef593c8793f47be
darkphantomke <46133578+darkphantomke@users.noreply.github.com>
2019-03-30 14:13:51 +0300

    vanilla2.0

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 5000;
-const host = process.env.HOST || 'localhost';
+//const port = process.env.PORT || 5000;
+//const host = process.env.HOST || 'localhost';
 const uri = 'mongodb://localhost:27017/twerk';
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+
+app.set('port', (process.env.PORT || 5000));
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -21,15 +23,15 @@ mongoose.connect(uri, { useNewUrlParser: true }, (err) => {
     }
 });
 
-/*app.get('*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
-});*/
+});
 
-http.createServer((req, res) => {
-    res.writeHead(200, app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '/public/index.html'));
-    }));
+/*http.createServer((req, res) => {
+    res.writeHead(200);
     res.end();
-  }), app.listen(port, host, () => 
-  console.log(`App started on port http://${host}:${port}`)
+});*/
+  
+app.listen(app.get('port'), () => 
+  console.log('App started on port https://localhost:' + app.get('port'))
 );
